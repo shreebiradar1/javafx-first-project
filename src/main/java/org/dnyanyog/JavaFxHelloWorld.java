efc80ff65bb3ab13a4640f9b8a93138e06641e68
Shree <shreebiradar2005@gmail.com>
2024-12-26 21:54:00 +0530

    Created first Javafx project.
    Just added the color in the background.

---
+++ src/main/java/org/dnyanyog/JavaFxHelloWorld.java
@@ -0,0 +1,31 @@
+package org.dnyanyog;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class JavaFxHelloWorld extends Application{
+	public static void main(String[] args) {
+		Application.launch();
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		Stage stage = new Stage();
+		
+		stage.setTitle("First Window");
+		stage.setFullScreen(true);
+		stage.setFullScreenExitKeyCombination(KeyCombination.valueOf("q"));
+		
+		Group actorGroup = new Group();
+		
+		Scene scene = new Scene(actorGroup, Color.RED);
+		
+		stage.setScene(scene);
+		stage.show();
+	}
+}
